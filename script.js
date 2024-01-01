1c1453892812c2354c05865a3c16f304b4c1bc55
Altay Hodoglugil <altay.hodo@gmail.com>
2024-01-01 13:21:56 -0800

    Add hover effects and change grid button

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 const gridContainer = document.querySelector('.grid-container');
+const gridButton = document.querySelector('.grid-button');
 
 function makeGrid(cols) {
   gridContainer.style.display = 'grid';
   gridContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
   for (let i = 0; i < (cols * cols); i++) {
     const gridItem = document.createElement('div');
-    gridItem.textContent = i;
     gridItem.style.border = '1px solid red';
+    gridItem.classList.add('grid-item');
     gridContainer.appendChild(gridItem);
   }
+  addGridEventListeners();
 }
 
-makeGrid(5);
+function clearGrid(){
+  gridContainer.innerHTML = '';
+}
+
+function addGridEventListeners(){
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(gridItem => {
+    gridItem.addEventListener('mouseover', updateHoverEffect);
+  });
+}
+
+makeGrid(100);
+
+function updateHoverEffect(e){
+  const square = e.target;
+  square.style.backgroundColor = 'red';
+}
+
+gridButton.addEventListener('click', updateGrid);
+
+function updateGrid(){
+  let gridSize = parseInt(prompt('How many columns'));
+  while(isNaN(gridSize) === true || gridSize > 100 || gridSize < 1){
+    gridSize = parseInt(prompt('Enter a number between 1 and 100'));
+  }
+  clearGrid();
+  makeGrid(parseInt(gridSize));
+}
