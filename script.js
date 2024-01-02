c8d1f2f0adc5ad2932938061df0c7fea4397aa99
Altay Hodoglugil <altay.hodo@gmail.com>
2024-01-01 21:06:48 -0800

    Add erase, rainbow, and clear buttons, and format page

--- script.js
+++ script.js
@@ -1,41 +1,77 @@
 const gridContainer = document.querySelector('.grid-container');
 const gridButton = document.querySelector('.grid-button');
+const colorMode = '';
+const eraseButton = document.querySelector('.erase-button');
+const rainbowButton = document.querySelector('.rainbow-button');
+const clearButton = document.querySelector('.clear-button');
 
 function makeGrid(cols) {
   gridContainer.style.display = 'grid';
   gridContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
   for (let i = 0; i < (cols * cols); i++) {
     const gridItem = document.createElement('div');
-    gridItem.style.border = '1px solid red';
     gridItem.classList.add('grid-item');
     gridContainer.appendChild(gridItem);
   }
   addGridEventListeners();
 }
 
-function clearGrid(){
+window.addEventListener('load', () => {
+  makeGrid(10);
+})
+
+function clearGrid() {
   gridContainer.innerHTML = '';
 }
 
-function addGridEventListeners(){
+function addGridEventListeners(colorMode = 'default') {
   const gridItems = document.querySelectorAll('.grid-item');
   gridItems.forEach(gridItem => {
-    gridItem.addEventListener('mouseover', updateHoverEffect);
+    gridItem.addEventListener('mouseover', (e) => {
+      addHoverEffect(e, colorMode);
+    })
+  });
+};
+
+
+eraseButton.addEventListener('click', () => {
+  addGridEventListeners('erase');
+});
+
+rainbowButton.addEventListener('click', () => {
+  addGridEventListeners('rainbow');
+})
+
+clearButton.addEventListener('click', eraseGrid);
+
+function eraseGrid(){
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(gridItem => {
+    gridItem.style.backgroundColor = 'white';
   });
 }
 
-makeGrid(100);
 
-function updateHoverEffect(e){
+function addHoverEffect(e, colorMode) {
   const square = e.target;
-  square.style.backgroundColor = 'red';
+  if(colorMode === 'erase'){
+    square.style.backgroundColor = 'white';
+  } else if(colorMode === 'rainbow'){
+    square.style.backgroundColor = getRandomRGB();
+  } else if(colorMode === 'default') {
+    square.style.backgroundColor = 'black';
+  }
+}
+
+function getRandomRGB(){
+  return `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }
 
 gridButton.addEventListener('click', updateGrid);
 
-function updateGrid(){
-  let gridSize = parseInt(prompt('How many columns'));
-  while(isNaN(gridSize) === true || gridSize > 100 || gridSize < 1){
+function updateGrid() {
+  let gridSize = parseInt(prompt('How many columns should there be?'));
+  while (isNaN(gridSize) === true || gridSize > 100 || gridSize < 1) {
     gridSize = parseInt(prompt('Enter a number between 1 and 100'));
   }
   clearGrid();
